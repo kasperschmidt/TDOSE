c280c15d2276e135423877adaac5e5dca56e4a42
Kasper Schmidt <kbschmidt@aip.de>
2018-03-14 08:34:57 +0100

    Update header and package handling fits files

--- tdose_modify_cube.py
+++ tdose_modify_cube.py
@@ -1,7 +1,7 @@
 # = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = =
 import numpy as np
 import sys
-import pyfits
+import astropy.io.fits as fits
 import tdose_utilities as tu
 import pdb
 # = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = =
@@ -28,25 +28,29 @@ def remove_object(datacube,sourcemodelcube,objects=[1],remove=True,dataext=0,sou
 
 
     """
-    if verbose: print ' - Loading data and source model cubes '
-    dataarr         = pyfits.open(datacube)[dataext].data
-    dataarr_hdr     = pyfits.open(datacube)[dataext].header
+    if verbose: print(' - Loading data and source model cubes ')
+    datacubehdu     = fits.open(datacube)
+    dataarr         = datacubehdu[dataext].data
+    dataarr_hdr     = datacubehdu[dataext].header
 
-    sourcemodel     = pyfits.open(sourcemodelcube)[sourcemodelext].data
-    sourcemodel_hdr = pyfits.open(sourcemodelcube)[sourcemodelext].header
+    sourcemodelhdu  = fits.open(sourcemodelcube)
+    sourcemodel     = sourcemodelhdu[sourcemodelext].data
+    sourcemodel_hdr = sourcemodelhdu[sourcemodelext].header
+    sourcemodelhdu.close()
+
     Nmodels         = sourcemodel.shape[0]
     models          = np.arange(Nmodels)
 
-    if verbose: print ' - Check that all objects indicated are present in source model cube'
+    if verbose: print(' - Check that all objects indicated are present in source model cube')
     objects  = np.asarray(objects)
 
     maxobj = np.max(np.abs(objects))
     if maxobj >= Nmodels:
         sys.exit(' ---> Object model "'+str(maxobj)+'" is not included in source model cube (models start at 0) ')
     else:
-        if verbose: print '   All object models appear to be included in the '+str(Nmodels)+' source models found in cube'
+        if verbose: print('   All object models appear to be included in the '+str(Nmodels)+' source models found in cube')
 
-    if verbose: print ' - Determining objects (source models) to remove from data cube '
+    if verbose: print(' - Determining objects (source models) to remove from data cube ')
     if remove:
         obj_remove = objects
         obj_keep   = np.setdiff1d(models,obj_remove)
@@ -59,23 +63,27 @@ def remove_object(datacube,sourcemodelcube,objects=[1],remove=True,dataext=0,sou
     modified_cube = dataarr - remove_cube
 
     if savecube:
+        datacubehdu[dataext].data = modified_cube # Replacing original data with modified cube
         outname = datacube.replace('.fits','_'+str(savecube)+'.fits')
-        if verbose: print ' - Saving modified cube to \n   '+outname
-        if verbose: print '   (Using datacube header with modification keywords appended) '
+        if verbose: print(' - Saving modified cube to \n   '+outname)
+        if verbose: print('   (Using datacube header with modification keywords appended) ')
         # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
-        hducube = pyfits.PrimaryHDU(modified_cube)       # default HDU with default minimal header
-        hducube.header = dataarr_hdr
+        # hducube = fits.PrimaryHDU(modified_cube)       # default HDU with default minimal header
+        # hducube.header = dataarr_hdr
 
-        # writing hdrkeys:     '---KEY--',                       '----------------MAX LENGTH COMMENT-------------'
-        hducube.header.append(('MODIFIED',                'True','Cube is modified with tdose_modify_cube.py?'),end=True)
-        hducube.header.append(('MODTIME ',   tu.get_now_string(),'Date and time of cube modification'),end=True)
-        hducube.header.append(('NREMOVE ',       len(obj_remove),'Number of sources removed from cube'),end=True)
-        hducube.header.append(('NKEEP   ',         len(obj_keep),'Number of sources kept in cube'),end=True)
-        hducube.header.append(('COMMENT ','Source models removed:'+','.join([str(oo) for oo in obj_keep])),end=True)
+        # adding hdrkeys:   '---KEY--',                       '----------------MAX LENGTH COMMENT-------------'
+        dataarr_hdr.append(('MODIFIED',                'True','Cube is modified with tdose_modify_cube.py?'),end=True)
+        dataarr_hdr.append(('MODTIME ',   tu.get_now_string(),'Date and time of cube modification'),end=True)
+        dataarr_hdr.append(('SMC     ',       sourcemodelcube,'Name of Source Model Cube'),end=True)
+        dataarr_hdr.append(('NREMOVE ',       len(obj_remove),'Number of sources removed from cube'),end=True)
+        dataarr_hdr.append(('NKEEP   ',         len(obj_keep),'Number of sources kept in cube'),end=True)
+        dataarr_hdr.append(('SREMOVE ',','.join([str(oo) for oo in obj_remove])),'Source indexes removed',end=True)
+        dataarr_hdr.append(('SKEEP   ',','.join([str(oo) for oo in obj_keep])),'Source indexes kept',end=True)
+        # dataarr_hdr.append(('COMMENT ','Source indexes removed:'+','.join([str(oo) for oo in obj_remove])),end=True)
+        # dataarr_hdr.append(('COMMENT ','Source indexes kept:'+','.join([str(oo) for oo in obj_keep])),end=True)
 
         # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
-        hdulist = pyfits.HDUList([hducube])       # turn header into to hdulist
-        hdulist.writeto(outname,clobber=clobber)  # write fits file (clobber=True overwrites excisting file)
+        datacubehdu.writeto(outname,overwrite=clobber)
 
     return modified_cube
 
